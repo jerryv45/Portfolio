5418d026eea9166b2a9f0ec6c0156c4c809b934a
Jerry <velasquezjerry98@gmail.com>
2022-10-06 21:23:04 -0700

    contact page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="contact flex flex-col justify-center place-items-center w-screen h-screen absolute left-0 top-0 border">
-  <h2 class="email text-3xl font-semibold lg:text-7xl cursor-pointer my-24 lg:my-40">Velasquezjerry98@gmail.com</h2>
+  <h2 class="email text-3xl md:text-5xl lg:text-7xl font-semibold cursor-pointer my-24 lg:my-40">Velasquezjerry98@gmail.com</h2>
   <ul class="link-list flex">
     <li class="mx-5 flex cursor-pointer lg:text-xl underline">Linkedin<div class="pl-2 w-6 h-6 grid place-items-center"><svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><title>Arrow Redo</title><path d="M448 256L272 88v96C103.57 184 64 304.77 64 424c48.61-62.24 91.6-96 208-96v96z" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"/></svg></div></li>
     <li class="mx-5 flex cursor-pointer lg:text-xl underline">Github<div class="pl-2 w-6 h-6 grid place-items-center"><svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><title>Arrow Redo</title><path d="M448 256L272 88v96C103.57 184 64 304.77 64 424c48.61-62.24 91.6-96 208-96v96z" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"/></svg></div></li>
