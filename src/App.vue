156b624195b80e425fd915b44040c37359aa0e26
Jerry <velasquezjerry98@gmail.com>
2022-10-14 00:23:59 -0700

    more things

--- src/App.vue
+++ src/App.vue
@@ -10,15 +10,16 @@
   </div>
     <div class="navbar sticky w-full flex flex-row justify-between m-0 p-0 mx-auto  "> 
       <p class="name">Jerry Velasquez</p>
-      <p class="occupation">Web Developer__</p>
+      <p class="occupation hidden md:block">Web Developer__</p>
 
-      <span class="theme-picker">
+      <span class="theme-picker flex">
         <p class="theme">Theme</p>
-        <ul>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+        <ul class="flex justify-center ">
+          <li id="el-1" class=" w-6 h-6 mx-[0.1rem] cursor-pointer border-2 border-gray-700 bg-[#2a2a2a]"></li>
+          <li class="el-2 w-6 h-6 mx-[0.1rem] cursor-pointer border-2 border-gray-700 bg-[#fc92f7]"></li>
+          <li class="el-3 w-6 h-6 mx-[0.1rem] cursor-pointer border-2 border-gray-700 bg-[#1F85DE]"></li>
+          <li class="el-4 w-6 h-6 mx-[0.1rem] cursor-pointer border-2 border-gray-700 bg-[#e52323]"></li>
+          
         </ul>
       </span>
 
